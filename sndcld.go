647931dbcbc21d7722201f2363294628b7446307
Nicolas Hillegeer <nicolas@hillegeer.com>
2014-07-01 12:40:01 +0200

    app: accept cmdline args
    Very simple, will probably use a command-style parser later.

--- sndcld.go
+++ sndcld.go
@@ -1,20 +1,45 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
 	"github.com/aktau/sndcld/snd"
 )
 
+var (
+	help = flag.Bool("help", false, "print help")
+)
+
+func init() {
+	flag.Parse()
+}
+
+func usage() {
+	fmt.Println("Usage: sndcld [-v] <url-of-track>")
+	os.Exit(0)
+}
+
 func main() {
+	if *help {
+		usage()
+	}
+
+	var soundUrl string
+	if flag.NArg() < 1 {
+		// usage()
+		// for debugging, we assign a predefined sound
+		soundUrl = "https://soundcloud.com/moullinex/discobelle-mix-041-moullinex"
+	} else {
+		soundUrl = flag.Arg(0)
+	}
+
 	fmt.Println("Welcome to sndcld. There is no sndcld though, only Zuul.")
 
 	client := snd.Client{Id: "22e566527758690e6feb2b5cb300cc43"}
-	url, err := client.Resolve("lol")
-	fmt.Println("got:", url, err)
-	soundUrl := "https://soundcloud.com/moullinex/discobelle-mix-041-moullinex"
-	url, err = client.Resolve(soundUrl)
+
+	url, err := client.Resolve(soundUrl)
 	if err != nil {
 		fmt.Printf("couldn't resolve sound %s: %v\n", soundUrl, err)
 		os.Exit(1)
